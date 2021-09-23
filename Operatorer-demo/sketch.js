a43afb06b2fb690f3d1b7ecca9a628002707ddf7
Lasse <lasse100204@gmail.com>
2021-09-23 15:01:00 +0200

    something works

--- Operatorer-demo/sketch.js
+++ Operatorer-demo/sketch.js
@@ -1,12 +1,38 @@
 
 
 function setup() {
-  createCanvas(400, 500);
+  createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
-circle(MouseX, MouseY,50);
+circle(mouseX, mouseY, 50);
   
+if (mouseX > 200 && mouseY < 200)
+fill("red")
+}
 
+function draw(){
+  background(220);
+circle(mouseX, mouseY, 50);
+{if (mouseX < 200 && mouseY > 200)
+  fill("red")
+}
+
+function draw() {
+  background(220);
+circle(mouseX, mouseY, 50);
+{if (mouseX > 200 && mouseY > 200)
+  fill("white")
+
+  function draw() {
+    background(220);
+  circle(mouseX, mouseY, 50);
+}
+
+function draw() {}
+  background(220);
+circle(mouseX, mouseY, 50);
+{if (mouseX < 200 && mouseY < 200)
+  fill("white")
 }
