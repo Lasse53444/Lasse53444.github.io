634f56c460692ed4dee4371cd97c80908b672f98
Lasse <lasse100204@gmail.com>
2021-09-23 14:16:41 +0200

    Ball actually bounces

--- bouncing-ball-demo/sketch.js
+++ bouncing-ball-demo/sketch.js
@@ -1,5 +1,7 @@
-let xPos = 40;
+let xPos = 30;
 let yPos = 200;
+let xSpeed = 2;
+let ySpeed = 2;
 
 function setup() {
   createCanvas(400, 400);
@@ -8,11 +10,23 @@ function setup() {
 function draw() {
   background(220);
 
-  
+  if (xSpeed ==2) 
+  fill(20)
+
 circle(xPos, yPos, 60);
   
-xPos += 1;
+yPos += ySpeed
+xPos += xSpeed
 
-if (xPos == 360)
- xPos -=320
+if (xPos > 370)
+ xSpeed = xSpeed * -1;
+
+ if (xPos < 30)
+ xSpeed = xSpeed +1;
+
+ if (yPos > 370)
+ ySpeed = ySpeed -1;
+
+ if (yPos < 30)
+ ySpeed = ySpeed +1;
 }
