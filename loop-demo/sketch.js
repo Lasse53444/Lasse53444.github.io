32cabab03347e10bc33e0cb9ed3a1cd1f2902dc5
Lasse <lasse100204@gmail.com>
2021-10-07 15:06:37 +0200

    opg 2 changes

--- loop-demo/sketch.js
+++ loop-demo/sketch.js
@@ -17,8 +17,15 @@ function setup() {
   }
   console.log("output opg 1");
 
-  for (let j = 1; j < 21; j += 1) {
+  //få j til at ændre sig i en løkke og brug den som variabel til j
+  let j = 0
+  let n = 0
+  let answer = n + j
+  while (j > 0) {
+    console.log("while demo" + j);
+
     j += 1
+
   }
-  console.log("output opg 2")
+  console.log("output opg 2");
 }
