0f2d58a93626c3c6ab60bb0b14b3d964a8638569
Lasse <lasse100204@gmail.com>
2021-10-07 14:51:51 +0200

    opg 1 done, working on opg 2

--- loop-demo/sketch.js
+++ loop-demo/sketch.js
@@ -6,4 +6,19 @@ function setup() {
 
   console.log("Loop demo");
 
+  console.log("while loop")
+
+  let i = 10
+
+  while (i > 0) {
+    console.log("while demo" + i);
+
+    i -= 1
+  }
+  console.log("output opg 1");
+
+  for (let j = 1; j < 21; j += 1) {
+    j += 1
+  }
+  console.log("output opg 2")
 }
