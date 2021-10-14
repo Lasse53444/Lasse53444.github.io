5057ab2a09d3e0d1b7313c214633b6ad18e9a443
Lasse <lasse100204@gmail.com>
2021-10-14 14:52:41 +0200

    Setup for event-demo

---
+++ event-demo/sketch copy.js
@@ -0,0 +1,12 @@
+
+let fillcolor = "black"
+
+function setup() {
+  createCanvas(WindowWidth, WindowHeight);
+}
+
+function draw() {
+  background(220);
+  fill(fillcolor)
+  circle(200, 200, 80)
+}
