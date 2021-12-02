baf2ea87bfc3c853064c056fddce9347d14a49fb
Lasse <lasse100204@gmail.com>
2021-12-02 14:42:13 +0100

    Day1

--- advent-of-code/day1/solutoin.js
+++ advent-of-code/day1/solutoin.js
@@ -5,3 +5,13 @@ const inputFileName = "opgave.txt"
 let content = fs.readFileSync(inputFileName).toString()
 
 console.log("Indhold:", content)
+
+let lines = content.split("\n").map(l => l.trim());
+
+console.log("Line count:", lines.length)
+console.log("Contents: ", lines)
+
+let a = Number("64)")
+let b = Number("77")
+let c = a + b
+console.log(c)
